4eb718d996ebaa2b2a336e5062a93351057773e6
Utsav Singh <singhutsavofficial@gmail.com>
2024-08-25 23:48:45 +0530

    remove depreceated parameters

--- platform_server/index.js
+++ platform_server/index.js
@@ -11,10 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
